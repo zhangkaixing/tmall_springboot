4a0ad195dd5c80794bb56b5179913f0670bf272c
zhang <1099205173@qq.com>
2020-10-19 10:39:42 +0800

    mall

--- src/main/java/com/how2java/tmall/dao/UserDAO.java
+++ src/main/java/com/how2java/tmall/dao/UserDAO.java
@@ -6,4 +6,8 @@ import com.how2java.tmall.pojo.User;
 
 public interface UserDAO extends JpaRepository<User,Integer>{
 
+    User findByName(String name);
+
+    User getByNameAndPassword(String name, String password);
+
 }
